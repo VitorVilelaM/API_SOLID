7690cba7a218738e29261bbaafe2233d466521fd
VitorVilelaM <vilelavitor305@gmail.com>
2024-10-15 11:23:20 -0300

    feat: Mudando arquitetura do projeto

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,25 @@
+import { registerUseCase } from '@/use-cases/register'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodyEsquema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodyEsquema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
