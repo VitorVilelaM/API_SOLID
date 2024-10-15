67c238492e4e8cb7e1afee6ed36ddadc504fea24
VitorVilelaM <vilelavitor305@gmail.com>
2024-10-15 19:24:29 -0300

    feat: implementação da busca pelo perfil do usuario

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,26 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFound } from './errors/resource-not-found-error'
+
+interface GetUserProfileUseCaseRequest {
+  userID: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userID,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userID)
+
+    if (!user) {
+      throw new ResourceNotFound()
+    }
+    return { user }
+  }
+}
